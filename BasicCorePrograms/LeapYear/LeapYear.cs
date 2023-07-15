48d5738f0b3120aa8164024bc411e4e4cee19729
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-16 01:44:44 +0530

    [mohitkumar9884] adding/creating program LeapYear

---
+++ BasicCorePrograms/LeapYear/LeapYear.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms.LeapYear
+{
+    internal class LeapYear
+    {
+        public void Year() 
+        {
+            
+            Console.WriteLine("Enter a year to check LeapYear");
+            int year =Convert.ToInt32(Console.ReadLine());
+            
+
+            bool isFourDigit = year >= 1000 && year <= 9999;
+            bool isDivisibleByFour = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+
+            if (isFourDigit && isDivisibleByFour)
+            {
+                Console.WriteLine("{0} is a 4-digit number and a Leap Year", year);
+            }
+            else if (isFourDigit)
+            {
+                Console.WriteLine("{0} is a 4-digit number, but not a Leap Year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a 4-digit number,enter a valid year", year);
+            }
+
+
+
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,23 +1,25 @@
 ﻿using BasicCorePrograms.FlipCoin;
+using BasicCorePrograms.LeapYear;
 
-namespace BasicCorePrograms
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-       static void Main(string[] args)
-       {
-            Console.WriteLine("Welcome To Basic Core Programs");
-            Console.WriteLine("Please chose any One program from below options");
-            Console.WriteLine("1.FlipCoin");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                    case 1:
-                    FlipPercentage flipPercentage = new FlipPercentage();
-                    flipPercentage.CoinToss();
-                    break;
-            }
-            
-       }
+        Console.WriteLine("Welcome To Basic Core Programs");
+        Console.WriteLine("Please chose any One program from below options");
+        Console.WriteLine("1.FlipCoin\n2.LeapYear");
+        Console.WriteLine(" ");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+                case 1:
+                FlipPercentage flipPercentage = new FlipPercentage();
+                flipPercentage.CoinToss();
+                break;
+                case 2:
+                LeapYear leapYear = new LeapYear();
+                leapYear.Year();
+                break;
+        }
     }
 }
