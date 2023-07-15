c59dfb795454decb01f13b6b9ac5209eea757b7c
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-16 04:28:13 +0530

    [mohitkumar9884] adding/creating  PowerOf2

---
+++ BasicCorePrograms/Power/PowerTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms.Power
+{
+    internal class PowerTable
+    {
+        public void NthPower()
+        {
+            
+            Console.WriteLine("Enter an integer(I) to get power of 2 (I<31): ");
+            int k=Convert.ToInt32(Console.ReadLine());
+
+            if(k>=31)
+            {
+                Console.WriteLine("Invalid input! k should be less than 31.");
+                return;
+            }
+
+
+            for (int N = 0; N<=k; N++)
+            {
+                int Z= (int)Math.Pow(2, N);
+                Console.WriteLine("Power of 2 for N = {0}:{1}", N, Z);
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BasicCorePrograms.FlipCoin;
 using BasicCorePrograms.LeapYear;
+using BasicCorePrograms.Power;
 
 internal class Program
 {
@@ -7,7 +8,7 @@ internal class Program
     {
         Console.WriteLine("Welcome To Basic Core Programs");
         Console.WriteLine("Please chose any One program from below options");
-        Console.WriteLine("1.FlipCoin\n2.LeapYear");
+        Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.PowerOf2");
         Console.WriteLine(" ");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
@@ -19,6 +20,10 @@ internal class Program
                 case 2:
                 LeapYear leapYear = new LeapYear();
                 leapYear.Year();
+                break;
+                case 3:
+                PowerTable powerTable= new PowerTable();
+                powerTable.NthPower();
                 break;
         }
     }
