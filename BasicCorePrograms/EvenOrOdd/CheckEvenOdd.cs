cc5c8c24b3e601cb43f20c5e0a361d849d12e4dc
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-16 23:44:36 +0530

    [mohitkumar9884] adding/modifying program EvenOdd

---
+++ BasicCorePrograms/EvenOrOdd/CheckEvenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms.EvenOrOdd
+{
+    internal class CheckEvenOdd
+    {
+        public void Calculate()
+        {
+            Console.WriteLine("Enter a valid number to check,if it is Even or Odd: ");
+            int number=Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2==0)
+            {
+                Console.WriteLine("Given number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Given number is Odd");
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -3,6 +3,7 @@ using BasicCorePrograms.LeapYear;
 using BasicCorePrograms.Power;
 using BasicCorePrograms.Harmonic;
 using BasicCorePrograms.NPrime;
+using BasicCorePrograms.EvenOrOdd;
 
 internal class Program
 {
@@ -10,7 +11,7 @@ internal class Program
     {
         Console.WriteLine("Welcome To Basic Core Programs");
         Console.WriteLine("Please chose any One program from below options");
-        Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.PowerOf2\n4.HarmonicNumber\n5.PrimeFactor");
+        Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.PowerOf2\n4.HarmonicNumber\n5.PrimeFactor\n6.EvenOdd");
         Console.WriteLine(" ");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
@@ -34,6 +35,10 @@ internal class Program
                 case 5:
                 PrimeFactor primeFactor = new PrimeFactor();
                 primeFactor.Factorize();
+                break;
+                case 6:
+                CheckEvenOdd checkEvenOdd = new CheckEvenOdd();
+                checkEvenOdd.Calculate();
                 break;
         }
     }
