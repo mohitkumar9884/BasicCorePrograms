4f22711dc75e6f6a949be40f836ca59585fcc53b
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-16 12:30:56 +0530

    [mohitkumar9884] adding/creating program HarmonicNumber

---
+++ BasicCorePrograms/Harmonic/HarmonicNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms.Harmonic
+{
+    internal class HarmonicNumber
+    {
+        public void Compute()
+        {
+            Console.WriteLine("Enter a Harmonic value of N(N!=0): ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N == 0)
+            {
+                Console.WriteLine("Provide a valid input");
+                return;
+            }
+            double harmonicValue = 0;
+            for(int i = 1; i <= N; i++)
+            {
+                harmonicValue += 1.0 / i;
+            }
+            Console.WriteLine("Nth Harmonic Value:{0}", harmonicValue);
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BasicCorePrograms.FlipCoin;
 using BasicCorePrograms.LeapYear;
 using BasicCorePrograms.Power;
+using BasicCorePrograms.Harmonic;
 
 internal class Program
 {
@@ -8,7 +9,7 @@ internal class Program
     {
         Console.WriteLine("Welcome To Basic Core Programs");
         Console.WriteLine("Please chose any One program from below options");
-        Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.PowerOf2");
+        Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.PowerOf2\n4.HarmonicNumber");
         Console.WriteLine(" ");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
@@ -24,6 +25,10 @@ internal class Program
                 case 3:
                 PowerTable powerTable= new PowerTable();
                 powerTable.NthPower();
+                break;
+                case 4:
+                HarmonicNumber harmonicNumber = new HarmonicNumber();
+                harmonicNumber.Compute();
                 break;
         }
     }
